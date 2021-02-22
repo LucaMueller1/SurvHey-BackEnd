6f1088190e16003c0cf843811930fbbb4b4b48ba
REIGNjs <unconfigured@null.spigotmc.org>
2021-02-22 09:45:56 +0100

    initial commit

---
+++ src/main/java/io/swagger/api/ApiException.java
@@ -0,0 +1,10 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-02-21T21:53:09.126Z[GMT]")
+public class ApiException extends Exception {
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
