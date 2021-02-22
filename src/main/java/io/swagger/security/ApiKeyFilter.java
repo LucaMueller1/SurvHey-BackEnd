ea33049e6e4b6761a4d073891341cc7965e88b21
lucamueller <Luca.Mueller1@ibm.com>
2021-02-22 15:34:05 +0100

    Enabled API-key authentication

---
+++ src/main/java/io/swagger/security/ApiKeyFilter.java
@@ -0,0 +1,25 @@
+package io.swagger.security;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ApiKeyFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private String principalRequestHeader;
+
+    public ApiKeyFilter(String principalRequestHeader) {
+        this.principalRequestHeader = principalRequestHeader;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(principalRequestHeader);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return null;
+    }
+
+}
---
+++ src/main/java/io/swagger/security/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package io.swagger.security;
+
+import io.swagger.api.PetApiController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+    //@Value("${yourapp.http.api_key}")
+    private static final String API_KEY_AUTH_HEADER_NAME = "api_key";
+
+    //@Value("${yourapp.http.auth-token}")
+    private String principalRequestValue = "test123";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        ApiKeyFilter filter = new ApiKeyFilter(API_KEY_AUTH_HEADER_NAME);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!principalRequestValue.equals(principal)) {
+                    log.info("API key invalid.");
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+
+        http.antMatcher("/**").
+                csrf().
+                disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and()
+                .addFilter(filter)
+                .authorizeRequests()
+                .antMatchers("/user/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
