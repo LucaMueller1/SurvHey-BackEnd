74191c6ea0ae8ab30c13a3b4cf9a37a5162e55a9
Nico Neuhaeuser <wi19018@lehre.dhbw-stuttgart.de>
2021-03-04 17:08:29 +0100

    -SurveyService added functions to get all surveys by an user and one specific survey. Additionally, to create a survey

--- src/main/java/io/swagger/services/SurveyService.java
+++ src/main/java/io/swagger/services/SurveyService.java
@@ -1,45 +1,123 @@
 package io.swagger.services;
 
-import io.swagger.model.AnswerOption;
+
 import io.swagger.model.Survey;
-import io.swagger.repository.SurveyRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
-import java.text.ParseException;
-import java.util.AbstractList;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Service
 public class SurveyService {
 
-    @Autowired
-    private SurveyRepository repository;
 
-    public Survey findById(Long id) {
-        Optional<Survey> survey = repository.findById(id);
+    private static SessionFactory factory;
 
-        if(survey.isPresent()) {
-            return survey.get();
-        } else {
-            return null;
+    public List<Survey> getAllSurveys(String EmailInput) {
+        try {
+
+            factory = new Configuration().configure().addAnnotatedClass(Survey.class).buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
 
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+
+        List<Survey> Results = null;
+        try {
+            tx = session.beginTransaction();
+            String query = "select * from SurvHey_DB.Survey where E_Mail = "+ EmailInput+" ;";
+            Results = session.createQuery(query, Survey.class).list();
+
+
+            tx.commit();
+            session.close();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return Results;
     }
 
-    public Survey findByUserEmail(String userEmail) {
-        //Survey survey = repository.findByEmail(userEmail);
-        return null;
+
+
+
+
+
+
+    public Survey getSurvey(long SurveyID_Input) {
+        try {
+
+            factory = new Configuration().configure().addAnnotatedClass(Survey.class).buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+
+        Survey Result = null;
+        try {
+            tx = session.beginTransaction();
+            String query = "select * from SurvHey_DB.Survey where Survey_ID = "+ SurveyID_Input+" ;";
+            Result = session.createQuery(query, Survey.class).getSingleResult();
+
+
+            tx.commit();
+            session.close();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return Result;
     }
 
-    public Survey addSurvey(Survey survey) {
-        Survey createdSurvey = repository.save(survey);
-        repository.flush();
 
-        return createdSurvey;
+    public Survey createSurvey(Survey Survey_Input) {
+        try {
+
+            factory = new Configuration().configure().addAnnotatedClass(Survey.class).buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        Session session = factory.openSession();
+
+
+        try {
+
+            session.save(Survey_Input);
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            if (session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+     return Survey_Input;
     }
 
 
--- src/main/java/io/swagger/services/UserService.java
+++ src/main/java/io/swagger/services/UserService.java
@@ -1,20 +1,9 @@
 package io.swagger.services;
 
-import io.swagger.model.User;
-import io.swagger.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class UserService {
 
-    @Autowired
-    private UserRepository repository;
-
-    public User findByEmail(String email) {
-        User user = repository.findByEmail(email);
-        return user;
-    }
 
 
 
