3bc1979e2ebef0ea8932ca25a0ed9a54c831f40b
Nico Neuhaeuser <wi19018@lehre.dhbw-stuttgart.de>
2021-03-15 17:34:55 +0100

    deleted testdata

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-//Testdata
-
-insert into survhey_db.User(E_Mail,Password,First_Name,Last_Name) values('Gur@ke.com','1234', 'Lord', 'Gurke');
-insert into survhey_db.User(E_Mail,Password,First_Name,Last_Name) values('Ka@rotte.de','hallo', 'Master', 'Karotte');
-insert into survhey_db.User(E_Mail,Password,First_Name,Last_Name) values('Ban@ane.net','bye', 'Sergant', 'Banane');
-insert into survhey_db.User(E_Mail,Password,First_Name,Last_Name) values('Ap@fel.info','start', 'Emperor', 'Apfel');
