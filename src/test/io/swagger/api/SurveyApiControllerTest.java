e7627419b5729b32dfc4bd6393c0929b66d58122
Niklas Baun <niklasbaun@gmail.com>
2021-03-31 12:30:52 +0200

    test cases added

--- src/test/io/swagger/api/SurveyApiControllerTest.java
+++ src/test/io/swagger/api/SurveyApiControllerTest.java
@@ -1,6 +1,9 @@
 package io.swagger.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.AnswerOption;
+import io.swagger.model.Submission;
 import io.swagger.model.Survey;
 import io.swagger.model.User;
 import io.swagger.services.*;
@@ -14,9 +17,16 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.UnsupportedEncodingException;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
@@ -64,39 +74,96 @@ class SurveyApiControllerTest {
     @Test
     void createSurvey() throws Exception {
 
+        Survey s1=new Survey(null,"Hallo","Hallo?","nps",null,null,u1,null);
+        MockHttpServletResponse response=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getStatus());
+        assertEquals(response.getStatus(),200);
 
+    }
+
+    @Test
+    void createSurveySubmission() throws Exception {
+
+        //set answeroptions
+        List<AnswerOption> answerOptions= new ArrayList<>();
+        AnswerOption answerOption1 = new AnswerOption(null,null,"answer1");
+        answerOptions.add(answerOption1);
+        AnswerOption answerOption2 = new AnswerOption(null,null,"answer2");
+        answerOptions.add(answerOption2);
+        AnswerOption answerOption3 = new AnswerOption(null,null,"answer3");
+        answerOptions.add(answerOption3);
+
+        Survey s1=new Survey(null,"Hallo","Hallo?","nps",null,null,u1,answerOptions);
+        MockHttpServletResponse response=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
+
+        submissionService.addOrUpdateSubmission(new Submission(null, s1.getId(), OffsetDateTime.now(), answerOptions, answerOption1, ???));
+
+        //look if works
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getStatus());
+        assertEquals(response.getStatus(),200);
+    }
+
+    @Test
+    void deleteSurveyById() throws Exception{
+        //create survey
         Survey s1=new Survey(null,"Hallo","Hallo?","nps",null,null,u1,null);
         MockHttpServletResponse response=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
         System.out.println(response.getContentAsString());
         System.out.println(response.getStatus());
 
+        //delete survey
+        MockHttpServletResponse response2=mockMvc.perform(MockMvcRequestBuilders.delete("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getStatus());
+        assertEquals(response.getStatus(),202);
     }
 
     @Test
-    void createSurveySubmission() {
-    }
+    void getSurveyAnalysisById() throws Exception{
 
-    @Test
-    void deleteSurveyById() {
-    }
+        Survey s1=new Survey(null,"Hallo","Hallo?","nps",null,null,u1,null);
+        MockHttpServletResponse response=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
 
-    @Test
-    void getSurveyAnalysisById() {
     }
 
     @Test
     void getSurveyById() {
+        //same as testing createSurvey
     }
 
     @Test
     void getSurveyResultsById() {
+        //part of testing createSurvey
     }
 
     @Test
     void getSurveySubmissionsById() {
+        //part of testing createSurvey
     }
 
     @Test
-    void updateSurveyById() {
+    void updateSurveyById() throws Exception {
+        //set answeroptions
+        List<AnswerOption> answerOptions= new ArrayList<>();
+        AnswerOption answerOption1 = new AnswerOption(null,null,"answer1");
+        answerOptions.add(answerOption1);
+        AnswerOption answerOption2 = new AnswerOption(null,null,"answer2");
+        answerOptions.add(answerOption2);
+        AnswerOption answerOption3 = new AnswerOption(null,null,"answer3");
+        answerOptions.add(answerOption3);
+
+        Survey s1=new Survey(null,"Hallo","Hallo?","nps",null,null,u1,answerOptions);
+        MockHttpServletResponse response=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getStatus());
+
+        surveyService.addOrUpdateSurvey(s1, "different","Hallo?","nps",null,null,u1,answerOptions);
+        MockHttpServletResponse response2=mockMvc.perform(post("/survey").header("'api_key':" ).contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(s1))).andReturn().getResponse();
+        System.out.println(response.getContentAsString());
+        System.out.println(response.getStatus());
+
+        //assertEquals(response.getContentAsString(), );
     }
 }
