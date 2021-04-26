81d9ea5282d0f3c773ae269dd04a3218a84f6fe1
lucamueller <Luca.Mueller1@ibm.com>
2021-04-26 11:00:33 +0200

    Packaged project again and the Dockerfile is now finally working yaay!

--- src/main/java/io/swagger/services/GeoLocationService.java
+++ src/main/java/io/swagger/services/GeoLocationService.java
@@ -4,6 +4,8 @@ import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
 import io.swagger.model.GeoIP;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -17,9 +19,9 @@ public class GeoLocationService {
 
     private DatabaseReader dbReader;
 
-    public GeoLocationService() throws IOException {
-        File database = new File("src/main/resources/GeoLite2-City_20210302/GeoLite2-City.mmdb");
-        dbReader = new DatabaseReader.Builder(database).build();
+    public GeoLocationService(@Value("classpath:/GeoLite2-City_20210302/GeoLite2-City.mmdb") Resource geoLiteCity) throws IOException {
+        //File database = new File("src/main/resources/GeoLite2-City_20210302/GeoLite2-City.mmdb");
+        dbReader = new DatabaseReader.Builder(geoLiteCity.getInputStream()).build();
     }
 
     public GeoIP getLocation(String ip) throws IOException, GeoIp2Exception {
